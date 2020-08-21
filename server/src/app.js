a9a4ccea7a1a68df605a697b80f2705de2359f56
Edgar779 <cherqezyancherqezyan97@gmail.com>
2020-08-20 23:18:56 -0700

    feature

--- server/src/app.js
+++ server/src/app.js
@@ -22,6 +22,8 @@ import methodOverride from 'method-override';
     }
 
     config () {
+      this.app.use(express.static(__dirname + "../../client/my-app/public/favicon.ico"))
+
       this.app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "X-Requested-With");
