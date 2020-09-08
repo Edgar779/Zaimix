d0203f306f6908528959ac96335c92ddafeafda6
Edgar779 <cherqezyancherqezyan97@gmail.com>
2020-09-08 19:48:59 +0400

    feature/Signin

--- client/my-app/src/components/signin.component.js
+++ client/my-app/src/components/signin.component.js
@@ -145,31 +145,6 @@ export default class Signin extends Component {
         </div>
       </div>
 
-      // <form onSubmit={this.handleSubmit}>
-      //  {!this.state.loginErr ?   <h3>Sign In</h3> : <h3 style={{color: 'red'}}>{this.state.loginErr}</h3>}
-
-      //     <div className="form-group">
-      //         <label>Email address</label>
-      //         <input type="email"  name="email" onChange={this.handleEmailChange} className="form-control" placeholder="Enter email" />
-      //     </div>
-
-      //     <div className="form-group">
-      //         <label>Password</label>
-      //         <input type="password" name="password" onChange={this.handlePasswordChange} className="form-control" placeholder="Enter password" />
-      //     </div>
-
-      //     <div className="form-group">
-      //         <div className="custom-control custom-checkbox">
-      //             <input type="checkbox" className="custom-control-input" id="customCheck1" />
-      //             <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-      //         </div>
-      //     </div>
-
-      //     <button type="submit" className="btn btn-primary btn-block">Submit</button>
-      //     <p className="forgot-password text-right">
-      //         Forgot <a href="#">password?</a>
-      //     </p>
-      // </form>
     );
   }
 }
