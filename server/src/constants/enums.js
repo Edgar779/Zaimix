1e524a19f94061b5838e786cd012f29778b45018
Edgar779 <cherqezyancherqezyan97@gmail.com>
2020-09-08 19:34:37 +0400

    feature/Roles

--- server/src/constants/enums.js
+++ server/src/constants/enums.js
@@ -1,8 +1,2 @@
-// export enum roleEnum {
-//     user = 1,
-//     admin
-//   }
-
-
 
 export const roleEnum = Object.freeze({superadmin: 1, admin: 2, operator: 3, verificator: 4, logist: 5, curier: 6})
