4433eddb4f1fa95b0f1389635017e921ef1f499f
Edgar779 <cherqezyancherqezyan97@gmail.com>
2020-09-08 19:46:13 +0400

    feature/LoadingIndicator

--- client/my-app/src/components/loadingIndicator.component.js
+++ client/my-app/src/components/loadingIndicator.component.js
@@ -10,8 +10,5 @@ export const LoadingIndicator = (props) => {
                 
             <Loader type="ThreeDots" color="#2BAD60" height="100" width="100" />
             </div>
-            
-        
-
     )
 }
